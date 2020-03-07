360acd71544ad02f873b621e61b1d19e25e01d34
LinguList <mattis.list@posteo.de>
2020-03-07 21:53:41 +0100

    update

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# Sui Dialect Research
+# Linguistic Survey of India
 
 Cite the source dataset as
 
-> Castro, Andy and Pan, Xingwen (2015): Sui dialect research. SIL: Guiyang.
+> Grierson, George Abraham (1928): Linguistic Survey of India. Comparative Vocabulary. Calcutta: Government of India Central Publication Branch.
 
 This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/ license
 
-
-Conceptlists in Concepticon:
-- [Castro-2015-608](https://concepticon.clld.org/contributions/Castro-2015-608)
 ## Notes
 
 
@@ -17,12 +14,20 @@ Conceptlists in Concepticon:
 ## Statistics
 
 
-![Glottolog: 0%](https://img.shields.io/badge/Glottolog-0%25-red.svg "Glottolog: 0%")
+![Glottolog: 88%](https://img.shields.io/badge/Glottolog-88%25-yellowgreen.svg "Glottolog: 88%")
 ![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
 ![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
 
-- **Varieties:** 0
-- **Concepts:** 0
-- **Lexemes:** 0
+- **Varieties:** 371
+- **Concepts:** 162
+- **Lexemes:** 66,249
 - **Sources:** 0
-- **Synonymy:** 0.00
+- **Synonymy:** 1.13
+
+## Possible Improvements:
+
+- Languages linked to [bookkeeping languoids in Glottolog](http://glottolog.org/glottolog/glottologinformation#bookkeepinglanguoids):
+  - TAKPA [tawa1289](http://glottolog.org/resource/languoid/id/tawa1289)
+
+
+- Entries missing sources: 66249/66249 (100.00%)
--- lexibank_lsi.py
+++ lexibank_lsi.py
@@ -4,29 +4,55 @@ import attr
 from pathlib import Path
 from pylexibank import Concept, Language
 from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank.util import pb
+from pylexibank import progressbar
 
 from lingpy import *
 from clldutils.misc import slug
 
-#@attr.s
-#class CustomConcept(Concept):
-#    Chinese_Gloss = attr.ib(default=None)
+@attr.s
+class CustomConcept(Concept):
+    PageNumber = attr.ib(default=None)
 
 
-#@attr.s
-#class CustomLanguage(Language):
+@attr.s
+class CustomLanguage(Language):
+    NameInSource = attr.ib(default=None)
+    
 
 
 class Dataset(BaseDataset):
     id = "lsi"
     dir = Path(__file__).parent
-    #concept_class = CustomConcept
-    #language_class = CustomLanguage
+    concept_class = CustomConcept
+    language_class = CustomLanguage
 
     def cmd_makecldf(self, args):
 
         files = sorted(self.raw_dir.glob('LSI_txt/*/*.txt'))
+        args.writer.add_sources()
+
+        # add concepts from list
+        concepts = {}
+        for concept in self.concepts:
+            cid = '{0}_{1}'.format(
+                    concept['NUMBER'],
+                    slug(concept['ENGLISH']))
+            args.log.info('adding {0}'.format(cid))
+            args.writer.add_concept(
+                    ID=cid,
+                    PageNumber=concept['PAGENUMBER'],
+                    Name=concept['ENGLISH'])
+            concepts[concept['PAGENUMBER']+' '+concept['ENGLISH']] = cid
+
+        # add languages from list
+        languages = {}
+        for language in progressbar(self.languages, desc='add languages'):
+            args.writer.add_language(
+                    ID=language['Name'],
+                    Name=language['Name'],
+                    Glottocode=language['Glottocode'],
+                    NameInSource=language['NameInSource'])
+            languages[slug(language['NameInSource'])] = language['Name']
 
         D = {
                 0: ['doculect', 'concept', 'number', 'form']
@@ -49,13 +75,35 @@ class Dataset(BaseDataset):
                         language = current_language
                     else:
                         current_language = language
-                    D[idx] = [language, concept, number, form]
-                    idx += 1
+                    if language.strip():
+                        D[idx] = [language, concept, number, form]
+                        idx += 1
         wl = Wordlist(D)
         wl.output(
                 'tsv',
                 prettify=False,
                 filename=self.raw_dir.joinpath('wordlist').as_posix()
                 )
+
+        missingc, missingl = set(), set()
+        for idx, doculect, concept, number, form in progressbar(wl.iter_rows(
+                'doculect', 'concept', 'number', 'form'), desc='cldfify'):
+            if concept in concepts and slug(doculect) in languages:
+                args.writer.add_form(
+                        Value=form,
+                        Form=form,
+                        Parameter_ID=concepts[concept],
+                        Language_ID=languages[slug(doculect)]
+                        )
+            else:
+                if concept not in concepts:
+                    missingc.add(concept)
+                if slug(doculect) not in languages:
+                    missingl.add(slug(doculect))
+        for m in missingc:
+            print(m)
+        print('')
+        for m in missingl:
+            print(m)
 
 
